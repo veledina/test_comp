f5ddaa321648137983d1a85b59e278ef41ee6841
Alexandra Veledina <alexandra.veledina@gmail.com>
2021-10-19 11:56:09 +0300

    Add files via upload

---
+++ plot_example.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x, y = np.loadtxt('example.txt', delimiter=',', unpack=True)
+plt.plot(x,y, label='Loaded from file!')
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Interesting Graph\nCheck it out')
+plt.legend()
+
+plt.savefig("example.png")
+
+plt.show()
